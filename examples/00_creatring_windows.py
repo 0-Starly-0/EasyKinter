94bcce2395c5a73e3d1359eccbf1de41d68d2e6e
0-Starly-0 <hlbassimoreira@gmail.com>
2026-08-26 11:32:04 -0300

    Refactor comments in 00_creatring_windows.py
    Updated comments for clarity and added explanations about tkinter usage.

--- examples/00_creatring_windows.py
+++ examples/00_creatring_windows.py
@@ -1,15 +1,16 @@
 import easykinter as ek
 import tkinter as tk
 
-# create the main tk.Tk window, but hide it
-# as we will not be working with it
+# Firstly, all tkinter scripts need the main tk.Tk window,
+# But who works with the root window anyways? Let's hide it.
 root = ek.CreateRoot(HideWindow=True)
 
-# creating the tk.Toplevel window
+# Let's start by creating the tk.Toplevel window:
 toplevel = ek.CreateToplevel("New toplevel!", root, None, 500, 500)
+# Yeah, it's all come down to just ONE LINE of code... ain't that neat?
 
-#centering the toplevel window
-ek.CenterWindow(toplevel)
-
-# the window can be also interactible with tkinter
+# The window can be also interacted with normally in tkinter!
 toplevel.title("I changed the toplevel's title!")
+
+# Almost as if... this was just tkinter all along, but better. Crazy concept, right?
+# Wink wink, nudge nudge, EasyKinter.
