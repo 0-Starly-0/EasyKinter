65071b9f4bca4137521d0bf23384f5c579ea20d9
0-Starly-0 <hlbassimoreira@gmail.com>
2026-08-26 11:13:15 -0300

    Update __init__.py

--- src/easykinter/__init__.py
+++ src/easykinter/__init__.py
@@ -1,3 +1,12 @@
+import os as _os
+import sys as _sys
+from pathlib import Path as _Path
+
+BIN_DIR = _Path(__file__).parent.joinpath("bin").resolve()
+if _sys.platform == "win32" and BIN_DIR.exists():
+    _os.add_dll_directory(str(BIN_DIR.resolve()))
+    _os.environ["PATH"] = str(BIN_DIR) + _os.pathsep + _os.environ["PATH"]
+
 from .EasyKinter import *
 
 import tkinter
