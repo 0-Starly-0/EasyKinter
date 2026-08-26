f98aaaa161e0e829cdc7925bfc39fcc61c2d4f23
0-Starly-0 <hlbassimoreira@gmail.com>
2026-08-26 11:43:28 -0300

    Refactor comments in refreshed_bind.py
    Updated comments and improved readability of the code.

--- examples/04_refreshed_bind.py
+++ examples/04_refreshed_bind.py
@@ -1,35 +1,37 @@
 import easykinter as ek
 import tkinter as tk
 
-# in tkinter, to bind things, it's so... boring...
+# As i'm sure we've all seen in tkinter, to bind things, it's so... boring...
 root = ek.CreateRoot(PosX=600, PosY=400)
 
-# you need to put event here, and it's so grueling!
+# You need to put event here, and it's so grueling! It's also really weird.
 def BindingFunction(event):
     root.destroy()
 
-# also, it's so oddly specific and nitpicked how you need to name thhe keybind!
-# having to use "<>" is so weird...
+# On top of that? It's so oddly specific and nitpicked on how you need to name thhe keybind!
+# Having to use "<>" is honestly a pretty bad design flaw in my opinion...
 root.bind("<Return>", BindingFunction)
 
-# forget it.
+# Just forget it all.
 
-# have you ever wished it was like THIS?
+# Have you ever wished it was like THIS?
 toplevel = ek.CreateToplevel()
 
-# no need to place and rely on event anymore!
+# Look at it: no need to place and rely on events anymore!
 def AnotherBindingFunction():
     toplevel.destroy()
 
-# you can name anything you want!
+# And also something that exsists in tkinter, but was brought back:
+# You can name any keybind you want! Literally to your heart's content!
 
-# individual keys:
+# Here, we have individual keys:
 ek.BetterBind(toplevel, KeyToBind="a", FunctionToBind=AnotherBindingFunction)
 
-# specific commands:
+# You can also do specific commands:
 ek.BetterBind(toplevel, KeyToBind="return", FunctionToBind=AnotherBindingFunction)
 
-# and no need to use <> anymore. but you also have freedom of choice:
+# Best of all? No need to use <> anymore.
+# (But you also have freedom of choice, i'm not a monster.)
 ek.BetterBind(toplevel, KeyToBind="<Return>", FunctionToBind=AnotherBindingFunction)
 
-# test it out! click on a window and press ENTER. (or A if you're in the toplevel.)
+# Test it out yourself! click on a window and press ENTER. (or A if you're focused in the toplevel window.)
