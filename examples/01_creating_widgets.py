2e712e993ee259e8dcb5b153048bee86514b5c1d
0-Starly-0 <hlbassimoreira@gmail.com>
2026-08-26 11:34:29 -0300

    Refactor comments in widget creation example
    Updated comments for clarity and consistency in the widget creation example.

--- examples/01_creating_widgets.py
+++ examples/01_creating_widgets.py
@@ -1,17 +1,17 @@
 import easykinter as ek
 import tkinter as tk
 
-# settings up the windows we will work with
+# Let's start this example by doing what we did beforehand:
 root = ek.CreateRoot(HideWindow=True)
 toplevel = ek.CreateToplevel("Cool widgets!", root, None, 500, 500)
 
-# creating a button
-# the button dimension is now measured in pixels! No more annoying MATH.
+# Remember how creating windows was as easy as a single line? You guessed it.
 button = ek.CreateButton(toplevel, "New\nButton", ButtonWidth=50, ButtonHeight=50)
+# The button dimension is now measured in pixels! No more annoying MATH.
 
-# you can do this to pack your widgets:
-button.place(relx=0.5, rely=0.55)
+# In normal (and unneficient) Tkinter, you usually wasted a line of code doing this:
+button.place(relx=0.5, rely=0.65)
 
-# creating a label
-# you can ALSO use the new implemented PackType to automatically pack the widgets!
+# Let me create a label to demonstrate the NEW WAY of doing it.
+# Because now- you can ALSO use the new implemented PackType to automatically pack the widgets!
 label = ek.CreateLabel(toplevel, "The widget below me is\na super cool Button!", PackType="Place", RelX=0.5, RelY=0.3)
