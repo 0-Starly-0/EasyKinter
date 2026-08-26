f6bb97e29aafef45ff4e452c0b783d3fce0503fd
0-Starly-0 <hlbassimoreira@gmail.com>
2026-08-26 11:36:12 -0300

    Refactor comments and code formatting in customizing_windows.py
    Updated comments and formatting for clarity and readability.

--- examples/02_customizing_windows.py
+++ examples/02_customizing_windows.py
@@ -1,25 +1,28 @@
 import easykinter as ek
 import tkinter as tk
 
-# previously in tkinter, customizing was pretty boring...
-# ... you needed to do a whole new line... for most things.
+# Previously in tkinter, customizing was pretty boring...
+# ... you needed to write a whole new line... for most things.
 root = ek.CreateRoot("Boring way...")
 
-# one line to change the colors...
+# Here, if we want to make out windows pretty-
+# We gotta write one line to change the colors...
 root.configure(bg="white")
 
 # ...one line to change their state...
 root.focus_force()
 
-# SO! MANY! LINES!
+# SO-
 root.wm_attributes("-alpha", 0.5)
+# MANY-
 root.wm_attributes("-topmost", True)
+# LINES OF CODE!
 root.resizable(False, False)
 
-# FORGET THIS!
+# YOU KNOW WHAT, FORGET THIS!
 root.withdraw()
 
 # Here, we have a simple tool to keep it easy.
 toplevel = ek.CreateToplevel("The EASY way!", root)
 ek.BetterConfigure(toplevel, background="white", WindowTrasparency=0.5, Topmost=True, ResizableHeight=False, ResizableWidth=False)
-# Whoo wee! Pretty nice!
+# Whoo wee! Pretty nice! It's looking pretty crisp now, ain't it?
